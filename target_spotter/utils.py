6f70b7eee847322878c79dd085a76b39fb26e96b
MiqG <miquelangladagirotto@gmail.com>
2023-01-31 14:41:21 +0100

    added function in utils to prepare the outputs to be inputted into spotter website

--- target_spotter/utils.py
+++ target_spotter/utils.py
@@ -18,6 +18,8 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 from joblib import Parallel, delayed
+import sqlalchemy
+import shutil
 
 GENE_LENGTHS_FILE = defaults.GENE_LENGTHS_FILE
 EXAMPLE_FILES = defaults.EXAMPLE_FILES
@@ -130,3 +132,65 @@ def load_examples(dataset="CCLE"):
     genexpr = pd.read_table(EXAMPLE_FILES[dataset]["genexpr"])
 
     return splicing, genexpr
+
+
+def save_sql(df, table_name, output_dir):
+    # create SQL database
+    url = os.path.join('sqlite:///'+output_dir,table_name+".sql")
+    print(url)
+    engine = sqlalchemy.create_engine(url, echo=False)
+    df.to_sql(table_name, con=engine, if_exists='replace')
+    print("Saved %s" % url)
+    
+            
+def prep_for_webapp(
+    splicing, genexpr, spldep, max_harm, pred_ic50, output_dir="webapp_inputs"
+):
+    # load references
+    info_event_gene = pd.read_table(defaults.MAPPING_FILE)
+    info_drugs = pd.read_table(defaults.INFO_DRUGS_FILE)
+    
+    # prepare
+    ## PSI
+    splicing.index.name = "EVENT"
+    splicing = pd.merge(
+            info_event_gene[["EVENT","GENE","ENSEMBL"]], splicing.reset_index(), 
+            how="inner", left_on="EVENT", right_on="EVENT"
+        )
+    ## gene expression
+    genexpr.index.name = "ENSEMBL"
+    genexpr = pd.merge(
+            info_event_gene[["GENE","ENSEMBL"]].drop_duplicates(), genexpr.reset_index(), 
+            how="inner", left_on="ENSEMBL", right_on="ENSEMBL"
+        )
+    ## splicing dependency and maximum harm scores
+    spldep.index.name = "EVENT"
+    spldep = pd.merge(
+        info_event_gene[["EVENT","GENE","ENSEMBL"]], spldep.reset_index(), 
+        how="inner", left_on="EVENT", right_on="EVENT"
+    )
+    max_harm.index.name = "EVENT"
+    max_harm = pd.merge(
+        info_event_gene[["EVENT","GENE","ENSEMBL"]], max_harm.reset_index(), 
+        how="inner", left_on="EVENT", right_on="EVENT"
+    )
+    ## predicted IC50s
+    pred_ic50 = pd.merge(
+        info_drugs[["DRUG_ID","DRUG_NAME","drug_screen","ID"]].drop_duplicates(), pred_ic50, 
+        how="inner", on="ID"
+    )
+    
+    # save
+    ## create webapp directory
+    os.makedirs(output_dir, exist_ok=True)
+    ## save as SQL
+    save_sql(splicing, "splicing", output_dir)
+    save_sql(genexpr, "genexpr", output_dir)
+    save_sql(spldep, "spldep", output_dir)
+    save_sql(max_harm, "max_harm", output_dir)
+    save_sql(pred_ic50, "pred_ic50", output_dir)
+    ## archive
+    shutil.make_archive(output_dir, "zip", output_dir)
+    ## remove directory
+    print("Compressing Webapp inputs...")
+    shutil.rmtree(output_dir, ignore_errors=True)
